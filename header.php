58484a2f11b7b1c25d63a2ee302803d3b49b6964
Unknown <greenwaets@gmail.com>
2017-01-18 12:19:19 +0100

    added the worst footer ever + icon

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
       <a href="profil.php">Profil</a>
   </nav>
 
-  <a href="#" class="profile">
+  <a href="profile.php" class="profile">
     <span class="name-and-balance"><?php session_start(); echo $_SESSION['username']; ?><br> $ 0,00</span>
     <img src="http://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/55/55e31493cc3bde5597be339f7b76327235027d86.jpg" alt="" class="avatar">
   </a>
--- index.php
+++ index.php
@@ -6,9 +6,14 @@
     <meta name="viewport" content="width=device-width">
 
     <!--style -->
-    <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Quattrocento+Sans:400,700,400italic|Montserrat:400,700|Cardo:400italic|Advent+Pro:100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
 </head>
@@ -46,6 +51,24 @@
         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
+</div>
+
+<div class="col-md-offset-1 col-md-6">
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda autem cum dolore dolorem doloribus ducimus ea eligendi et eum numquam quaerat, repellendus sapiente, sunt suscipit ullam vitae voluptate, voluptatibus!
+
+</div>
+<div class="col-md-4">
+
+
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-twitter"></i></a>
+        <a href="#"><i class="fa fa-youtube"></i></a>
+        <a href="#"><i class="fa fa-linkedin"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam assumenda autem cum dolore dolorem doloribus ducimus ea eligendi et eum numquam quaerat, repellendus sapiente, sunt suscipit ullam vitae voluptate, voluptatibus!
+
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
