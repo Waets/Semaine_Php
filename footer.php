8a13d6f6d04c34e03b7fb538322c6e530f2a7041
adam <adambenhaddou@gmail.com>
2017-01-18 12:50:10 +0100

    Add footer

---
+++ footer.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+    <title>EXO2</title>
+    <meta charset="">
+    <meta name="viewport" content="width=device-width">
+
+    <!--style -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css">
+    <link href="css/stylefooter.css" rel="stylesheet">
+
+
+</head>
+<body>
+
+
+<div id="footer">
+  <div id="utility-links">
+
+   <div class="help">
+   <ul>
+     <h3 class="left">Need Help?</h3>
+     <li><a href="#">Check Order Status</a></li>
+     <li><a href="#">Return Policy</a></li>
+     <li><img class="help-img-icon" width="18" height="14" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469462241/msg_jn0whe.svg"</img><a href="#">Live Chat</a></li>
+     <li><img class="help-img-icon" width="15" height="15" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469462243/phone_zfj4ey.svg"</img><a href="#">1-888-922-2336</a></li>
+   </ul>
+     </div>
+ 
+  
+    <div class="rectangle"></div>
+
+   <div class="about">
+        <ul>
+          <h3 class="left">About Academy</h3>
+          <li><a href="#">Company Information</a></li>
+          <li><a href="#">Careers</a></li>
+       </ul>
+       </div>
+  
+    <div class="rectangle"></div>
+  
+  <div class="card">
+        <ul>
+          <h3 class="left">Card Services</h3>
+          <li><a href="#">Academy Credit Card</a></li>
+          <li><a href="#">Academy Gift Card</a></li>
+       </ul>
+   </div>
+</div>  
+  
+<div id="newsletter-social">  
+    <div id="newsletter">
+    <h3 class="center">Get More Deals!</h3>
+    <p class="deals">Weekly ad, new store openings, store events, and more.</p>
+    <form>
+    <input type="submit" value="Sign Up">
+    </form>
+</div>  
+    
+    
+ <div id="social">
+    <a href="http://facebook.com/academy" target="_blank">
+    <img width="34" height="34" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469457641/facebook_khedl5.svg"></a></img>
+
+    <a href="http://twitter.com/academy" target="_blank">
+    <img width="34" height="34" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469457641/twitter_fu5ejk.svg"></a></img>
+
+    <a href="hhttp://pinterest.com/academy/" target="_blank">
+    <img width="34" height="34" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469457641/pinterest_kahh1i.svg"></a></img>
+
+    <a href="http://www.youtube.com/user/academydotcom" target="_blank">
+    <img width="34" height="34" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469457641/youtube_xrntt3.svg"></a></img>
+
+    <a href="http://instagram.com/academy" target="_blank">
+    <img width="34" height="34" src="http://res.cloudinary.com/cloudinary-account/image/upload/v1469457641/instagram_ugek0w.svg"></a></img>
+
+  </div>  
+</div>  
+     
+<div class="rectangle-horiz"></div>
+
+<div id="copyright">
+<ul>
+  <li class="copy">© 2016 Academy Sports + Outdoors. All Rights Reserved.</li>
+
+  <li class="copy">Privacy Policy | Terms + Conditions | Sitemap</li>
+
+  <li class="copy">California Proposition 65 | California Transparency in Supply Chains Act (SB 657)</li>
+  
+</div>
+  
+   </div>
+
+</footer>
+</body>
+
+</html>
