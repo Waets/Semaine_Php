af2ba0f3b4f4c470f1de632ac984b78f60322cef
Unknown <greenwaets@gmail.com>
2017-01-20 10:36:09 +0100

    Somethings

---
+++ course.php
@@ -0,0 +1,12 @@
+<?php include "header.php" ?>
+<link href="css/course.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<div class="card card-5">
+    <h2>League of Legends</h2>
+    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis facere fuga nemo. A alias at dignissimos
+        doloremque exercitationem explicabo fugiat labore, magnam maiores modi non officiis omnis quaerat soluta
+        ullam?</h4>
+    <button class="button">Acheter</button>
+</div>
--- store.php
+++ store.php
@@ -1,4 +1,5 @@
-<?php include "header.php" ?><html>
+<?php include "header.php" ?>
+<html>
 <head>
     <title>EXO2</title>
     <meta charset="">
@@ -6,7 +7,8 @@
 
     <!--style -->
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css">
     <link href="css/stylestore.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
@@ -14,105 +16,78 @@
 
 </head>
 <body>
-  <div class="container">
+<div class="container">
     <div class="layout-positioner">
-      <div class="menu2">
-        <ul class="list2">
-          <li><a href="#">Populaire</a></li>
-          <li><a href="#">Jeux</a>
-            <ul class="drop2">
-              <li><a href="#">Libre</a></li>
-              <li><a href="#">Accès anticipé</a></li>
-              <li><a href="#">Demo</a></li>
-              <li><a href="#">Réalité Virtuelle</a></li>
-              <li>Recherche par genre</li>
-              <li><a href="#">Course</a></li>
-              <li><a href="#">Indie</a></li>
-              <li><a href="#">Casual Game</a></li>
-              <li><a href="#">ММО</a></li>
-              <li><a href="#">Jeu d'aventure</a></li>
-              <li><a href="#">Roleplay</a></li>
-              <li><a href="#">Simulateur</a></li>
-              <li><a href="#">Jeu de sport</a></li>
-              <li><a href="#">Strategie</a></li>
-              <li><a href="#">Action</a></li>
-              <li> Recherche par plate-forme</li>
-              <li><a href="#">MAC OS X</a></li>
-              <li><a href="#">Steam OS et Linux</a></li>
+        <div class="menu2">
+            <ul class="list2">
+                <li><a href="#">Populaire</a></li>
+                <li><a href="#">Jeux</a>
+                    <ul class="drop2">
+                        <li><a href="#">Libre</a></li>
+                        <li><a href="#">Accès anticipé</a></li>
+                        <li><a href="#">Demo</a></li>
+                        <li><a href="#">Réalité Virtuelle</a></li>
+                        <li>Recherche par genre</li>
+                        <li><a href="#">Course</a></li>
+                        <li><a href="#">Indie</a></li>
+                        <li><a href="#">Casual Game</a></li>
+                        <li><a href="#">ММО</a></li>
+                        <li><a href="#">Jeu d'aventure</a></li>
+                        <li><a href="#">Roleplay</a></li>
+                        <li><a href="#">Simulateur</a></li>
+                        <li><a href="#">Jeu de sport</a></li>
+                        <li><a href="#">Strategie</a></li>
+                        <li><a href="#">Action</a></li>
+                        <li> Recherche par plate-forme</li>
+                        <li><a href="#">MAC OS X</a></li>
+                        <li><a href="#">Steam OS et Linux</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Programmes</a></li>
+                <li><a href="#">Appareils</a></li>
+                <li><a href="#">Vidéo</a></li>
+                <li><a href="#">Nouvelles</a></li>
+                <li><a href="#">Pour vous</a></li>
             </ul>
-          </li>
-          <li><a href="#">Programmes</a></li>
-          <li><a href="#">Appareils</a></li>
-          <li><a href="#">Vidéo</a></li>
-          <li><a href="#">Nouvelles</a></li>
-          <li><a href="#">Pour vous</a></li>
-        </ul>
-      </div>
-      <!-- menu2 -->
-      <div class="search">
-        <form action="https://echo.htmlacademy.ru" method="post">
-          <input type="text" name="login">
-        </form>
-        <!-- search -->
-      </div>
-     <div class="leftmenu">
-<ul>
-  <span>Parcourir les catégories</span>
-  <li><a href="#">Libre</li>
-  <li><a href="#">Accès précoce</li>
-  <li><a href="#">Course</li>
-  <li><a href="#">Casual Game</li>
-  <li><a href="#">ММО</li>
-  <li><a href="#">Jeu d'aventure</li>
-  <li><a href="#">Roleplay</li>
-  <li><a href="#">Simulateur</li>
-  <li><a href="#">Jeu de sport</li>
-  <li><a href="#">Stratégie</li>
-  <li><a href="#">Action</li>
-  </ul></div>
-      <!-- layout -->
-    </div>  
-     <div class="layout-positioner">
-     <div class="slider"><a href="#"><img src="http://i.piccy.info/i9/df19c8e92c78b253439516073ba69d37/1464790839/86509/1020259/gta.jpg"></img></a>
-         <div class="layout-positioner">
-     <div class="slider"><a href="#"><img src="http://i.piccy.info/i9/df19c8e92c78b253439516073ba69d37/1464790839/86509/1020259/gta.jpg"></img></a>
-
-       <!-- slider -->
+        </div>
+        <!-- menu2 -->
+        <div class="search">
+            <form action="https://echo.htmlacademy.ru" method="post">
+                <input type="text" name="login">
+            </form>
+            <!-- search -->
+        </div>
+        <div class="leftmenu">
+            <ul>
+                <span>Parcourir les catégories</span>
+                <li><a href="#">Libre</li>
+                <li><a href="#">Accès précoce</li>
+                <li><a href="#">Course</li>
+                <li><a href="#">Casual Game</li>
+                <li><a href="#">ММО</li>
+                <li><a href="#">Jeu d'aventure</li>
+                <li><a href="#">Roleplay</li>
+                <li><a href="#">Simulateur</li>
+                <li><a href="#">Jeu de sport</li>
+                <li><a href="#">Stratégie</li>
+                <li><a href="#">Action</li>
+            </ul>
+        </div>
+        <!-- layout -->
     </div>
-    <div class="sale"><a href="#"><img src="http://i.piccy.info/i9/ec29ca95fb451fd1a9b48d66a8175d00/1464792848/155793/1020259/a.png"></img></a>
-    <div class="sale_info"><span>Vente de la semaine.</span><p> Meilleur vente de la semaine<p>
-Jeu en promotion jusqu'à la procaine démarche.<p> 20:00 heure de Paris.</div>
-       <!-- sale -->
-    <!-- container -->
-  </div>  
-<footer class="footer-distributed ">
-        <div class="footer-left ">
-            <h3>TiBO<span>IPTV</span></h3>
-            <div>
-                <p class="footer-company-name ">TiBO IPTV &copy; 2015</p>
-            </div>
-        </div>
-        <div class="footer-center ">
-            <div>
-                <i class="fa fa-map-marker "></i>
-                <p><span>Blv Gjergj Fishta , Pll G&P,Kati II 1001 </span> Tirane, Albania</p>
-            </div>
-            <div>
-                <i class="fa fa-phone "></i>
-                <p>+355 67 600 67 67</p>
-            </div>
-            <div>
-                <i class="fa fa-envelope "></i>
-                <p><a href="mailto:info@tibo.tv ">info@tibo.tv</a></p>
-            </div>
-        </div>
-        <div class="footer-right ">
-            <p class="footer-company-about ">
-                <span>About the company</span>
-                Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
-            </p>
-        </div>
-    </footer>
+    <div class="layout-positioner">
+        <div class="slider"><a href="#"><img
+                        src="http://i.piccy.info/i9/df19c8e92c78b253439516073ba69d37/1464790839/86509/1020259/gta.jpg"></img></a>
+                <div class="sale"><a href="#"><img
+                                src="http://i.piccy.info/i9/ec29ca95fb451fd1a9b48d66a8175d00/1464792848/155793/1020259/a.png"></img></a>
+                    <div class="sale_info"><span>Vente de la semaine.</span>
+                        <p> Meilleur vente de la semaine
+                        <p>
+                            Jeu en promotion jusqu'à la procaine démarche.
+                        <p> 20:00 heure de Paris.</div>
+                    <!-- sale -->
+                    <!-- container -->
+                </div>
 </body>
 
 </html>
