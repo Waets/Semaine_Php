5f1dccd8b68e12739045054af2f1ad7fe1dbe8fb
Unknown <greenwaets@gmail.com>
2017-01-20 09:41:20 +0100

    indented everything

--- panier.php
+++ panier.php
@@ -10,35 +10,75 @@
     <link rel="stylesheet" type="text/css">
     <link href="css/stylestore.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
+    <link href="css/panier.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
 </head>
-
 <div class="container">
     <div class="row">
-        <div class="col-md-4">Mon panier</div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">
-            <img src="..." alt="..." class="img-rounded">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, asperiores commodi cupiditate debitis delectus ea enim eveniet facere impedit, inventore laudantium minus natus pariatur qui velit vero voluptas? Accusamus, laborum.
+        <!-- Boxes de Acoes -->
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+            <div class="box">
+                <div class="icon">
+                    <div class="image"><i class="fa fa-thumbs-o-up"></i></div>
+                    <div class="info">
+                        <h3 class="title">Made with Bootstrap</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien. Phasellus ultrices gravida massa luctus ornare. Suspendisse blandit quam elit, eu imperdiet neque semper.
+                        </p>
+                        <div class="more">
+                            <a href="#" title="Title Link">
+                                Read More <i class="fa fa-angle-double-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="space"></div>
+            </div>
         </div>
-        <div class="col-md-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias cum deserunt ex explicabo inventore perferendis similique veritatis. Ab atque consequuntur, delectus harum iste minus nam porro quaerat saepe tempore.
-        </div>
-        <div class="col-md-4">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur enim non reprehenderit. Cum dolor fugiat fugit maiores, minus numquam omnis perspiciatis ratione sapiente sequi, soluta suscipit tenetur. Mollitia, officia, optio!
-        </div>
-    </div>
-    <div class="panel panel-default">
-        <div class="panel-body">A Basic Panel</div>
-    </div>
-    <div class="panel panel-default">
-        <div class="panel-body">A Basic Panel</div>
-    </div><div class="panel panel-default">
-        <div class="panel-body">A Basic Panel</div>
-    </div>
 
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+            <div class="box">
+                <div class="icon">
+                    <div class="image"><i class="fa fa-flag"></i></div>
+                    <div class="info">
+                        <h3 class="title">Icons by Font Awesome</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien. Phasellus ultrices gravida massa luctus ornare. Suspendisse blandit quam elit, eu imperdiet neque semper.
+                        </p>
+                        <div class="more">
+                            <a href="#" title="Title Link">
+                                Read More <i class="fa fa-angle-double-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="space"></div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-lg-4">
+            <div class="box">
+                <div class="icon">
+                    <div class="image"><i class="fa fa-desktop"></i></div>
+                    <div class="info">
+                        <h3 class="title">Desktop Friendly</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in lobortis nisl, vitae iaculis sapien. Phasellus ultrices gravida massa luctus ornare. Suspendisse blandit quam elit, eu imperdiet neque semper.
+                        </p>
+                        <div class="more">
+                            <a href="#" title="Title Link">
+                                Read More <i class="fa fa-angle-double-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="space"></div>
+            </div>
+        </div>
+        <!-- /Boxes de Acoes -->
+    </div>
+</div>
 </body>
 
 </html>
